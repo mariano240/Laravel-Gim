87e8fb38375d4b8bd2b29f8796dede71854a7065
Palacios Mariano <mariano24.palacios@gmail.com>
2018-09-11 12:58:25 -0300

    Se genero modelo, controlador y migrate de tipo_pago

---
+++ app/Http/Controllers/TipoPagoController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\TipoPago;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class TipoPagoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\TipoPago  $tipoPago
+     * @return \Illuminate\Http\Response
+     */
+    public function show(TipoPago $tipoPago)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\TipoPago  $tipoPago
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(TipoPago $tipoPago)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\TipoPago  $tipoPago
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, TipoPago $tipoPago)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\TipoPago  $tipoPago
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(TipoPago $tipoPago)
+    {
+        //
+    }
+}
